ee2465863595e2a9182075380f52e75feea962fc
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-10 14:39:43 +0530

    feat: add a simple health check endpoint

--- src/server.js
+++ src/server.js
@@ -5,6 +5,12 @@ import app from "./app.js";
 
 const PORT = process.env.PORT;
 
+app.get('/health', (req, res) => {
+    res.status(200).json({
+        "status": "ok"
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
