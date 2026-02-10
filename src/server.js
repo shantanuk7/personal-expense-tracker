a7ceac4f6d5758f08b8eb169902675d9f1414eec
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-10 14:52:28 +0530

    refactor: move health check endpoint to app.js

--- src/server.js
+++ src/server.js
@@ -5,12 +5,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT;
 
-app.get('/health', (req, res) => {
-    res.status(200).json({
-        "status": "ok"
-    });
-});
-
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
