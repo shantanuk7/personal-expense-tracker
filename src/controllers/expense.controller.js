8ffecb4eac1a6d9af69962c64d285d22abea85f7
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 13:09:58 +0530

    feat: update expense controller to get userId from request header integer data type

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -3,6 +3,21 @@
 import { getAllExpenses } from "../services/expense.service.js";
 
 export const getExpenses = async (req,res)=> {
-    const expenses = await getAllExpenses();
-    res.status(200).json(expenses);
+    try {
+        const userId = parseInt(req.header('userId'));
+        console.log(userId);
+        
+        if(isNaN(userId)){
+            throw new Error("Invlaid userId. Please enter a valid integer.")
+        }
+        
+        const expenses = await getAllExpenses(userId);
+        res.status(200).json(expenses);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({
+            "code":"INVALID_REQUEST_DATA",
+            "message": error.message || "Unexpected request data."
+        });
+    }
 };
