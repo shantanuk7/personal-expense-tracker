21f8fbf8db46d48f57cee02577af82f9f99dc7a5
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 15:28:36 +0530

    feat: add filtering by category

--- src/repositories/expense.repository.js
+++ src/repositories/expense.repository.js
@@ -2,6 +2,29 @@
 
 import prisma from "../config/db.js";
 
-export const getExpenses = async ()=>{
+const getCategories = async ()=>{
+    return await prisma.category.findMany();
+}
+
+export const getExpenses = async (userId, filters)=>{
+    // First getting the categories list
+
+    const { category } = filters;
+
+    console.log("Filtering by category: "+ category);
+
+    // const dbCategories = await getCategories();
+
+    const expenses = await prisma.expense.findMany({
+        where: {
+            user_id: {
+                equals: userId
+            },
+            category: {
+                equals: category.toUpperCase()
+            }
+        }
+    })
+
     return await prisma.expense.findMany();
 }
