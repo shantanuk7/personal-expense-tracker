7b448e61dfdad3b5c5118bf02c7d8334321e6c74
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 10:17:08 +0530

    feat: add get expenses method in expense repository

--- src/models/expense.model.js
+++
@@ -1,1 +0,0 @@
-// src/expense.model.js
--- src/repositories/expense.repository.js
+++ src/repositories/expense.repository.js
@@ -1,1 +1,7 @@
 // src/expense.repository.js
+
+import prisma from "../config/db.js";
+
+export const getExpenses = async ()=>{
+    return await prisma.expense.findMany();
+}
