7cc83f4b4529c5776e392e4b0d365be1bfb07ccc
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 10:21:31 +0530

    feat: add router in the express app.js file

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 // src/app.js
 
 import express from "express";
+import router from "./routes/expense.route.js";
+
 const app = express();
+
+app.use(express.json());
+
+// Expense Routes
+app.use("/api",router);
 
 app.get('/health', (req, res) => {
     res.status(200).json({
