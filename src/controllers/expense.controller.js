caf9ee7400a16c563bfc4c75cb50cf824d6183a0
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 10:19:18 +0530

    feat: add the GET /expenses route handler method in expense controller

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -1,1 +1,8 @@
 // src/expense.controller.js
+
+import { getAllExpenses } from "../services/expense.service.js";
+
+export const getExpenses = async (req,res)=> {
+    const expenses = await getAllExpenses();
+    res.status(200).json(expenses);
+};
