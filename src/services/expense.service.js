91eef7a97aee8bf7194b643a315cc608b6ea0c03
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 10:17:42 +0530

    feat: add get all expenses method in the expense service

--- src/services/expense.service.js
+++ src/services/expense.service.js
@@ -1,1 +1,7 @@
 // src/expense.service.js
+
+import { getExpenses } from "../repositories/expense.repository.js";
+
+export const getAllExpenses = async () => {     
+    return await getExpenses();
+}
