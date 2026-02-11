acedeb0f01a39569b6dedba85b1c6be042a8b4c4
shantanuk7 <shantanukulkarni365@gmail.com>
2026-02-11 13:06:40 +0530

    feat: import getUniqueUser method in expense service

--- src/services/expense.service.js
+++ src/services/expense.service.js
@@ -1,7 +1,18 @@
 // src/expense.service.js
 
 import { getExpenses } from "../repositories/expense.repository.js";
+import { getUniqueUser } from "../repositories/user.repository.js";
 
-export const getAllExpenses = async () => {     
+export const getAllExpenses = async (userId) => {
+
+    // Checking if the user exists before fetching expenses
+    const userExists = await getUniqueUser(userId);
+
+    if (!userExists) {
+        const error = new Error(`User with ID ${userId} does not exist`);
+        error.status = 404;
+        throw error;
+    }
+
     return await getExpenses();
 }
